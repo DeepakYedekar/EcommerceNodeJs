0df2d5d72a9561e77d3dc512dd56a325a1a58c79
Deepak <deepakyedekar28@gmail.com>
2022-09-01 17:00:02 +0530

    bug fix

--- index.js
+++ index.js
@@ -1,96 +1,90 @@
-const express=require('express');
-const {serverPort}=require('./config/server.config');
-const {categoriresRoutes,ProductsRoutes,AuthRoutes,CartRoutes}=require('./routes');
-const {sequelize,Categories,Products,Role,User}=require('./models');
-require('dotenv').config();
-const app=express();
+const {serverPort} = require('./config/server.config')
+const express = require('express')
+const { Categories, sequelize, Products, Role,User } = require('./models')
+const {categoryRoutes, productRoutes, authRoutes, cartRoutes} = require('./routes')
+const app = express()
 
-app.use(express.json());
-app.use(categoriresRoutes);
-app.use(ProductsRoutes);
-app.use(AuthRoutes);
-app.use(CartRoutes);
+app.use(express.json())
+app.use(authRoutes)
+app.use(categoryRoutes)
+app.use(productRoutes)
+app.use(cartRoutes)
 
-
-
-
-app.listen(serverPort,async ()=>{
-    console.log(`server started on port ${serverPort}`);
-    await sequelize.sync({force:true});
-   await init();
-});  
-
-
+app.listen(serverPort, async ()=> {
+	console.log('server is running on this port', serverPort)
+	await init()
+})
 
 async function init(){
-
-let cat=[
-    {
-        name:"Shade",
-        description:"About Shade"
-    },
-    {
-        name:"Home",
-        description:"About Home"
-    },
-    {
-        name:"Park",
-        description:"About Park"
-    }
-]
-
-let pro=[
-    {
-        name:"bag",
-        description:"Mens & Womens Shoes",
-        cost:"600",
-        quantity:"100",
-        CategoryId:"1"
-    },
-    {
-        name:"necles",
-        description:"Mens & Womens Shoes",
-        cost:"600",
-        quantity:"100",
-        CategoryId:"1"
-    },
-    {
-        name:"HairBands",
-        description:"Mens & Womens Shoes",
-        cost:"600",
-        quantity:"100",
-        CategoryId:"1"
-    },
-    {
-        name:"Trimmer",
-        description:"Mens & Womens Shoes",
-        cost:"600",
-        quantity:"100",
-        CategoryId:"1"
-    }
-]
-
-let defaultRole=[
-    {
-        name:"User"
-    },
-    {
-        name:"Admin"
-    }
-]
-
-let UserRole=[
-    {
-        "username":"abc123",
-         "password":"abc123",
-         "email":"abc123@gmail.com",
-         "roles":[1,2]
-     
-     }
-]
-
-await Categories.bulkCreate(cat);
-await Products.bulkCreate(pro);
-await Role.bulkCreate(defaultRole);
-await User.bulkCreate(UserRole);
-}
+	await sequelize.sync({force:true});
+	
+	let cat=[
+		{
+			name:"Shade",
+			description:"About Shade"
+		},
+		{
+			name:"Home",
+			description:"About Home"
+		},
+		{
+			name:"Park",
+			description:"About Park"
+		}
+	]
+	
+	let pro=[
+		{
+			name:"bag",
+			description:"Mens & Womens Shoes",
+			cost:"600",
+			quantity:"100",
+			CategoryId:"1"
+		},
+		{
+			name:"necles",
+			description:"Mens & Womens Shoes",
+			cost:"600",
+			quantity:"100",
+			CategoryId:"1"
+		},
+		{
+			name:"HairBands",
+			description:"Mens & Womens Shoes",
+			cost:"600",
+			quantity:"100",
+			CategoryId:"1"
+		},
+		{
+			name:"Trimmer",
+			description:"Mens & Womens Shoes",
+			cost:"600",
+			quantity:"100",
+			CategoryId:"1"
+		}
+	]
+	
+	let defaultRole=[
+		{
+			name:"User"
+		},
+		{
+			name:"Admin"
+		}
+	]
+	
+	let UserRole=[
+		{
+			"username":"abc123",
+			 "password":"abc123",
+			 "email":"abc123@gmail.com",
+			 "roles":[1,2]
+		 
+		 }
+	]
+	
+	Categories.bulkCreate(cat);
+	Products.bulkCreate(pro);
+	Role.bulkCreate(defaultRole);
+	User.bulkCreate(UserRole);
+	}
